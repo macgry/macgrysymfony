eb1538c4e3bc6828d88e85ee48ac791ea841ddf5
Maciej Grygierzec <maciej.grygierzec@student.uj.edu.pl>
2026-05-27 07:14:09 +0000

    crud dla postow

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -1,97 +1,46 @@
 <?php
 
-/**
- * Post controller.
- */
-
 namespace App\Controller;
 
 use App\Entity\Post;
 use App\Form\Type\PostType;
 use App\Service\PostServiceInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\FormType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
-/**
- * Class PostController.
- */
 #[Route('/post')]
 class PostController extends AbstractController
 {
-    /**
-     * Constructor.
-     *
-     * @param PostServiceInterface $postService Post service
-     * @param TranslatorInterface  $translator  Translator
-     */
     public function __construct(
         private readonly PostServiceInterface $postService,
         private readonly TranslatorInterface $translator
     ) {
     }
 
-    /**
-     * Index action.
-     *
-     * @param int $page Page number
-     *
-     * @return Response HTTP response
-     */
-    #[Route(
-        name: 'post_index',
-        methods: ['GET']
-    )]
+    #[Route(name: 'post_index', methods: ['GET'])]
     public function index(#[MapQueryParameter] int $page = 1): Response
     {
-        $pagination = $this->postService->getPaginatedList($page);
-
         return $this->render('post/index.html.twig', [
-            'pagination' => $pagination,
+            'pagination' => $this->postService->getPaginatedList($page),
         ]);
     }
 
-    /**
-     * View action.
-     *
-     * @param Post $post Post entity
-     *
-     * @return Response HTTP response
-     */
-    #[Route(
-        '/{id}',
-        name: 'post_view',
-        requirements: ['id' => '[1-9]\d*'],
-        methods: ['GET']
-    )]
+    #[Route('/{id<\d+>}', name: 'post_view', methods: ['GET'])]
     public function view(Post $post): Response
     {
-        return $this->render(
-            'post/view.html.twig',
-            ['post' => $post]
-        );
+        return $this->render('post/view.html.twig', [
+            'post' => $post,
+        ]);
     }
 
-    /**
-     * Create action.
-     *
-     * @param Request $request HTTP request
-     *
-     * @return Response HTTP response
-     */
-    #[Route(
-        '/create',
-        name: 'post_create',
-        methods: ['GET', 'POST'],
-    )]
+    #[Route('/create', name: 'post_create', methods: ['GET', 'POST'])]
     public function create(Request $request): Response
     {
         $post = new Post();
-
         $form = $this->createForm(PostType::class, $post);
 
         $form->handleRequest($request);
@@ -99,111 +48,52 @@ class PostController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $this->postService->save($post);
 
-            $this->addFlash(
-                'success',
-                $this->translator->trans('message.created_successfully')
-            );
+            $this->addFlash('success', $this->translator->trans('message.created_successfully'));
 
             return $this->redirectToRoute('post_index');
         }
 
-        return $this->render(
-            'post/create.html.twig',
-            ['form' => $form->createView()]
-        );
+        return $this->render('post/create.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 
-    /**
-     * Edit action.
-     *
-     * @param Request $request HTTP request
-     * @param Post    $post    Post entity
-     *
-     * @return Response HTTP response
-     */
-    #[Route(
-        '/{id}/edit',
-        name: 'post_edit',
-        requirements: ['id' => '[1-9]\d*'],
-        methods: ['GET', 'PUT']
-    )]
+    #[Route('/{id}/edit', name: 'post_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Post $post): Response
     {
-        $form = $this->createForm(
-            PostType::class,
-            $post,
-            [
-                'method' => 'PUT',
-                'action' => $this->generateUrl('post_edit', [
-                    'id' => $post->getId(),
-                ]),
-            ]
-        );
+        $form = $this->createForm(PostType::class, $post, [
+            'action' => $this->generateUrl('post_edit', ['id' => $post->getId()]),
+        ]);
 
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $this->postService->save($post);
 
-            $this->addFlash(
-                'success',
-                $this->translator->trans('message.edited_successfully')
-            );
+            $this->addFlash('success', $this->translator->trans('message.edited_successfully'));
 
             return $this->redirectToRoute('post_index');
         }
 
-        return $this->render(
-            'post/edit.html.twig',
-            [
-                'form' => $form->createView(),
-                'post' => $post,
-            ]
-        );
+        return $this->render('post/edit.html.twig', [
+            'form' => $form->createView(),
+            'post' => $post,
+        ]);
     }
 
-    /**
-     * Delete action.
-     *
-     * @param Request $request HTTP request
-     * @param Post    $post    Post entity
-     *
-     * @return Response HTTP response
-     */
-    #[Route(
-        '/{id}/delete',
-        name: 'post_delete',
-        requirements: ['id' => '[1-9]\d*'],
-        methods: ['GET', 'DELETE']
-    )]
+    #[Route('/{id}/delete', name: 'post_delete', methods: ['GET', 'POST'])]
     public function delete(Request $request, Post $post): Response
     {
-        $form = $this->createForm(FormType::class, $post, [
-            'method' => 'DELETE',
-            'action' => $this->generateUrl('post_delete', [
-                'id' => $post->getId(),
-            ]),
-        ]);
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
+        if ($request->isMethod('POST')) {
             $this->postService->delete($post);
 
-            $this->addFlash(
-                'success',
-                $this->translator->trans('message.deleted_successfully')
-            );
+            $this->addFlash('success', $this->translator->trans('message.deleted_successfully'));
 
             return $this->redirectToRoute('post_index');
         }
 
-        return $this->render(
-            'post/delete.html.twig',
-            [
-                'form' => $form->createView(),
-                'post' => $post,
-            ]
-        );
+        return $this->render('post/delete.html.twig', [
+            'post' => $post,
+        ]);
     }
 }
--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -1,50 +1,31 @@
 <?php
 
-/**
- * Post entity.
- */
-
 namespace App\Entity;
 
 use App\Repository\PostRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
 
-/**
- * Class Post.
- */
 #[ORM\Entity(repositoryClass: PostRepository::class)]
 #[ORM\Table(name: 'posts')]
 class Post
 {
-    /**
-     * Primary key.
-     */
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column(type: 'integer')]
     private ?int $id = null;
 
-    /**
-     * Title.
-     */
     #[ORM\Column(type: 'string', length: 255)]
     private ?string $title = null;
 
-    /**
-     * Created at.
-     */
     #[ORM\Column(type: 'datetime_immutable')]
+    #[Gedmo\Timestampable(on: 'create')]
     private ?\DateTimeImmutable $createdAt = null;
 
-    /**
-     * Updated at.
-     */
     #[ORM\Column(type: 'datetime_immutable')]
+    #[Gedmo\Timestampable(on: 'update')]
     private ?\DateTimeImmutable $updatedAt = null;
 
-    /**
-     * Category.
-     */
     #[ORM\ManyToOne(targetEntity: Category::class, fetch: 'EXTRA_LAZY')]
     #[ORM\JoinColumn(nullable: false)]
     private ?Category $category = null;
@@ -69,14 +50,14 @@ class Post
         return $this->createdAt;
     }
 
-    public function setCreatedAt(?\DateTimeImmutable $createdAt): void
-    {
-        $this->createdAt = $createdAt;
-    }
-
     public function getUpdatedAt(): ?\DateTimeImmutable
     {
         return $this->updatedAt;
+    }
+
+    public function setCreatedAt(?\DateTimeImmutable $createdAt): void
+    {
+        $this->createdAt = $createdAt;
     }
 
     public function setUpdatedAt(?\DateTimeImmutable $updatedAt): void
@@ -92,7 +73,6 @@ class Post
     public function setCategory(?Category $category): static
     {
         $this->category = $category;
-
         return $this;
     }
 }
